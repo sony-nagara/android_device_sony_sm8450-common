01c8ce0f20debd196b714668c9e0b55eabe1ed86
Tuan Anh <tuan73176@gmail.com>
2025-05-04 11:32:20 +0700

    sm8450-common: Import GPS configurations
    XQ-CT72-user 14 64.2.A.2.244 064002A002024400519766968 release-keys
    
    Change-Id: If1fa3ebe9937c1cb3d9d6a3238876ec7808dfa22

--- common.mk
+++ common.mk
@@ -161,6 +161,9 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
 # Graphics
