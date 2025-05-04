cf6ec2c85f6930fc3c96148a6e4a07c14763d7e3
Tuan Anh <tuan73176@gmail.com>
2025-05-04 11:27:16 +0700

    sm8450-common: Import thermal configs
    XQ-CT72-user 14 64.2.A.2.244 064002A002024400519766968 release-keys
    
    Change-Id: I9083e8c45f6f7f4b6abdca3f9a7f2574344b2ec4

--- common.mk
+++ common.mk
@@ -345,6 +345,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_ODM)/etc/thermal-engine.conf
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
