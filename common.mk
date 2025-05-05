c00306a778937f343a5d724cc24432c40cd85279
Tuan Anh <tuan73176@gmail.com>
2025-05-05 19:18:31 +0700

    sm8450-common: Sync liblight with Sagami
    Change-Id: I9da875330b7f63ad8b686235d260d757a22581b3

--- common.mk
+++ common.mk
@@ -194,6 +194,10 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.sony
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
