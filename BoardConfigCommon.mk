537dff6dacb0184e21d6fa804e6584def0326375
Alexander Koskovich <akoskovich@pm.me>
2025-05-05 22:46:37 +0700

    sm8450-common: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,7 +76,7 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
