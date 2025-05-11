2e6fc8e048b224cb9998de07080c1534f77b1d6d
someone5678 <someone5678@users.noreply.github.com>
2025-05-11 12:01:36 +0700

    sm8450-common: Use QCOM USB AIDL gadget HAL
    * Newer SoC's like taro use gadget HAL
    * Import usb_compositions.conf for later customization
    
    Change-Id: Ib3383bc1173db22c539b2073d72419c42fcf92cc

--- common.mk
+++ common.mk
@@ -193,8 +193,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.sony.rc \
     init.target.rc \
     ueventd.qcom.rc
@@ -361,10 +359,16 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/usb_compositions.conf:$(TARGET_COPY_OUT_VENDOR)/etc/usb_compositions.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
