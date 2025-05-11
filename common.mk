c2df3cc49ddeb8fa4011dca7ae3eb1485acb0a66
Arian <arian.kulmer@web.de>
2025-05-11 11:48:46 +0700

    sm8450-common: Enable 64bit audio HAL
    Change-Id: Iab35e98a78863c8bb08d07b3fc3d13b792e4f824

--- common.mk
+++ common.mk
@@ -31,6 +31,8 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
+$(call soong_config_set, android_hardware_audio, run_64bit, true)
+
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
