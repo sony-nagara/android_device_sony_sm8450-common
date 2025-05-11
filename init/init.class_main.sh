526204c7f305937db992deaa42933636c3e075d8
ad1tyas1ngh <adityasingh1768@gmail.com>
2025-05-11 13:35:09 +0700

    sm8450-common: init: Remove qmiproxy service
    Change-Id: I74a22f6ab3dea297db049e17596b2fd365cc9bb0
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- init/init.class_main.sh
+++ init/init.class_main.sh
@@ -31,7 +31,6 @@
 # start ril-daemon only for targets on which radio is present
 #
 baseband=`getprop ro.baseband`
-sgltecsfb=`getprop persist.vendor.radio.sglte_csfb`
 datamode=`getprop persist.vendor.data.mode`
 low_ram=`getprop ro.config.low_ram`
 qcrild_status=true
@@ -92,19 +91,6 @@ case "$baseband" in
     else
         start vendor.ril-daemon
     fi
-
-    case "$baseband" in
-        "svlte2a" | "csfb")
-          start qmiproxy
-        ;;
-        "sglte" | "sglte2" )
-          if [ "x$sgltecsfb" != "xtrue" ]; then
-              start qmiproxy
-          else
-              setprop persist.vendor.radio.voice.modem.index 0
-          fi
-        ;;
-    esac
 
     multisim=`getprop persist.radio.multisim.config`
 
