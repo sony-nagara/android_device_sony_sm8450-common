a672220cdadd0bb55c3f209eb093974fd8933907
Luofan Chen <clfbbn@gmail.com>
2025-05-19 22:40:10 +0700

    sm8450-common: Add rkp-V3-ndk to secd and keyprovd service
    Change-Id: I4f6240d9d9617d7be12933d8cd0c33b2cb70dcae

--- extract-files.py
+++ extract-files.py
@@ -53,6 +53,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+    ('vendor/bin/hw/vendor.semc.hardware.secd@1.1-service', 'vendor/bin/keyprovd'): blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     'vendor/bin/thermal-engine-v2': blob_fixup()
         .binary_regex_replace(b'oem/etc/thermal-engine.conf', b'odm/etc/thermal-engine.conf'),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
