bfc96cc51aeddb97a285ab05f1284aaecc521925
Alexander Koskovich <akoskovich@pm.me>
2025-05-20 10:07:49 +0700

    sm8450-common: Set BOARD_USES_GENERIC_KERNEL_IMAGE
     * We don't really use GKI but our target is a 'GKI'
       one according to Google. Useful for this guard in
       system/core;
    
       LOCAL_POST_INSTALL_CMD := mkdir -p $(addprefix $(TARGET_RAMDISK_OUT)/,$(my_ramdisk_dirs))
       ifeq (true,$(BOARD_USES_GENERIC_KERNEL_IMAGE))
           LOCAL_POST_INSTALL_CMD += $(addprefix $(TARGET_RAMDISK_OUT)/first_stage_ramdisk/,$(my_ramdisk_dirs))
       endif
    
    Change-Id: I0d79ab24eafeb0fe74d5963538b6bcc3e85edff6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,7 @@ BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_CONFIG := \
