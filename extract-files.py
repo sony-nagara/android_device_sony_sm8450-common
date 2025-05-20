9147a8cb57ef960ceba35bedef47c357400f4785
Łukasz Patron <priv.luk@gmail.com>
2025-05-20 20:46:13 +0700

    sm8450-common: Shim libwfdnative with libinput_shim/libbinder_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5

--- extract-files.py
+++ extract-files.py
@@ -59,6 +59,9 @@ blob_fixups: blob_fixups_user_type = {
 	.add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
 	.add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+	.add_needed('libbinder_shim.so')
+        .add_needed('libinput_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     ('vendor/bin/hw/android.hardware.gnss-aidl-service-qti', 'vendor/lib64/hw/android.hardware.gnss-aidl-impl-qti.so'): blob_fixup()
